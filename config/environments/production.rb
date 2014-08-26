32acde2aad5cc289b2c954394d34c64a31eebdad
Sidhartha Madhavaram <sidhu.abhi91@gmail.com>
2014-08-25 17:00:17 -0700

    Update new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-SampleApp:Application.configure do
+Sample_App:Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
